ae1aa085270acda45f315e5ebf5a9a2feece03ec
kyle <altck8@gmail.com>
2016-03-17 00:02:28 -0700

    added helper to convert create_at to a more standard format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  define_method :time_conversion do |whole|
+    midnight = "AM"
+    whole_date = whole.to_s( )[0..10]
+    date = whole_date.split( '-' )
+    fixed_date = ( date[1] + '-' + date[2] + '-' + date[0] ).split( " " )
+
+    whole_time = whole.to_s( )[11..18]
+    whole_time = whole_time.split( ":" )
+    if whole_time[0].to_i( ) > 13
+      midnight = "PM"
+      whole_time[0] = whole_time[0].to_i( ) - 12
+      whole_time[0] = whole_time[0].to_s
+    end
+    whole_time = whole_time[0] + ":" + whole_time[1] + ":" + whole_time[2]
+    return fixed_date.join( ) + " " + whole_time + midnight
+  end
 end
